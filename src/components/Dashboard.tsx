a9eab30475d39e76c8d1fa9b9c4f8d8aaa5a420f
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2022-09-17 18:09:33 +0200

    Add react-bootstrap

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 import styles from '../styles/Dashboard.module.css';
 
 function Dashboard() {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-            <span className={styles.myText}>fOO</span>
-            <h2>HELLO THERE</h2>
+            <span className={styles.myText}>HELLO WORLD</span>
+            <Button>Button</Button>
         </div>
     );
 }
